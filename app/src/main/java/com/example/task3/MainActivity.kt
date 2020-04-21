9d17ac75f1897f6bc9b0fa59078c933131bf790a
MyStart.ng <precyrisca@gmail.com>
2020-04-21 23:46:04 +0100

    Final Commit

--- app/src/main/java/com/example/task3/MainActivity.kt
+++ app/src/main/java/com/example/task3/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.example.task3
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        facebook.setOnClickListener {
+            intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse("http://www.facebook.com"))
+            startActivity(intent)
+
+        }
+        linkedin.setOnClickListener {
+            intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse("http://www.linkedin.com"))
+            startActivity(intent)
+        }
+        twitter.setOnClickListener {
+            intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse("http://www.twitter.com"))
+            startActivity(intent)
+        }
+        instagram.setOnClickListener {
+            intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse("http://www.instagram.com"))
+            startActivity(intent)
+        }
+        google.setOnClickListener {
+            intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse("http://www.google.com"))
+            startActivity(intent)
+        }
     }
 }
